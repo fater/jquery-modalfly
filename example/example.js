a649f444a7708f09c9b3b720121303fa5725f4b9
Chaikin Evgenii <fat_hack@rambler.ru>
2016-11-04 01:29:55 +0700

    Correction text and description

--- example/example.js
+++ example/example.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('[data-action="modalfly_example"]').click(function () {
         var obj = {};
         obj.title = 'Заголовок окна';
-        obj.content = 'Из JS мф задаем содержимое этой формы<br />' +
+        obj.content = 'Из JS мы задаем содержимое этой формы<br />' +
             '<input class="form-control" type="text" data-trigger="enter" data-name="data" autofocus /> ' +
             ' Задаем элементу формы тригер на срабатывание на этом поле при нажатии клавиши Enter, Задаем имя поля, чтобы отправить его со значением на сервер. А так же добавляем атрибут автофокуса' +
             '<p><input type="checkbox" data-trigger="enter" data-name="checkbox1" value="val2" autofocus /> ' +
