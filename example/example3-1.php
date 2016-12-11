be77945adaf9e5bda9f1e7acf9200e6cb24d1427
Chaikin Evgenii <fat_hack@rambler.ru>
2016-12-11 16:51:56 +0700

    Updated Examples files

---
+++ example/example3-1.php
@@ -0,0 +1,4 @@
+<?php
+$json = ['assign_element' => ['#example3-2' => 'New content',]];
+
+echo json_encode($json);
--- example/example3.php
+++ example/example3.php
@@ -1,8 +1,9 @@
 <?php
-$json = [];
-$json['assign_element'] = [
-    '#example3-1' => 'Content 1',
-    '#example3-2' => 'Content 2',
+$json = [
+    'assign_element' => [
+        '#example3-1' => 'Content 1',
+        '#example3-2' => '<a href="javascript:;" data-module="modalfly" data-url="example3-1.php">Get new dynamic content</a>',
+    ]
 ];
 
 echo json_encode($json);
